0cde745ad74a39f44a8ed845329c97745068561e
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-27 13:49:22 +0100

    Added routing, kendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import * as Cosmos from "@azure/cosmos";
+import { RouterOutlet, Router } from '@angular/router';
+import { homeIcon, SVGIcon } from '@progress/kendo-svg-icons';
+import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
+import { KENDO_ICONS } from "@progress/kendo-angular-icons";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KENDO_BUTTONS, KENDO_ICONS],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ExBatch-Heumann';
+  public svgHome: SVGIcon = homeIcon;
 
-  async click() {
-    let endpoint = "https://schruefer.documents.azure.com:443/";
-    let key = "ZE8r1ZNlJuL7o1F10F5NuPlJgJiC2TElldQycH2QCxIaZzkGcnxA5Za3URdElQM8ef66ctGmLNz1ACDbc9JuIA";
-    let client = new Cosmos.CosmosClient({endpoint: endpoint, key: key});
-    let database = "Heumann";
-    let db = client.database(database);
-    let container = db.container("Categories");
-    try {
-      await container.items
-      .query({
-          query: "SELECT * from c"
-      })
-      .fetchAll()
-      .then((response: any) => {
-        console.log(response.resources);
-      }) 
-    } catch(error) {
-      console.log(error);
-    }    
+  constructor( private router: Router ) {
+   
   }
+  
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
 }
