4c09493c29c3aca142209664c95af5307ed6ce90
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-27 14:15:33 +0100

    Reset to Cosmos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import * as Cosmos from "@azure/cosmos";
+import { RouterOutlet, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,26 +8,13 @@ import * as Cosmos from "@azure/cosmos";
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ExBatch-Heumann';
 
-  async click() {
-    let endpoint = "https://schruefer.documents.azure.com:443/";
-    let key = "ZE8r1ZNlJuL7o1F10F5NuPlJgJiC2TElldQycH2QCxIaZzkGcnxA5Za3URdElQM8ef66ctGmLNz1ACDbc9JuIA";
-    let client = new Cosmos.CosmosClient({endpoint: endpoint, key: key});
-    let database = "Heumann";
-    let db = client.database(database);
-    let container = db.container("Categories");
-    try {
-      await container.items
-      .query({
-          query: "SELECT * from c"
-      })
-      .fetchAll()
-      .then((response: any) => {
-        console.log(response.resources);
-      }) 
-    } catch(error) {
-      console.log(error);
-    }    
+  constructor( private router: Router ) {
+   
   }
+  
+  goHome() {
+    this.router.navigate(['/']);
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BatchreleaseComponent } from './batchrelease/batchrelease.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'batchrelease', component: BatchreleaseComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
