eb79ecb0ff7545668ec2ffd886c2add39934f73b
Bernd Maass <bernd.maass_ext@uid.com>
2025-01-27 14:04:02 +0100

    Kendo weg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, Router } from '@angular/router';
-import { homeIcon, SVGIcon } from '@progress/kendo-svg-icons';
-import { KENDO_BUTTONS } from "@progress/kendo-angular-buttons";
-import { KENDO_ICONS } from "@progress/kendo-angular-icons";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, KENDO_BUTTONS, KENDO_ICONS],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  public svgHome: SVGIcon = homeIcon;
 
   constructor( private router: Router ) {
    
